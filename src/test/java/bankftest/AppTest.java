62ae053ae25fb82cddeaddc7ff0dbb6ffcd1f7f5
ashnike <ashnilnaik7@gmail.com>
2023-06-04 13:10:04 +0530

    made changes

---
+++ src/test/java/bankftest/AppTest.java
@@ -0,0 +1,15 @@
+package bankftest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	  public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
