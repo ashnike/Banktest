4165b4e0632f6b291ef3cb96ad434fc2bfefede1
Ashnil <85815731+ashnike@users.noreply.github.com>
2023-06-04 14:22:25 +0530

    Update App.java

--- src/main/java/bankftest/App.java
+++ src/main/java/bankftest/App.java
@@ -3,6 +3,7 @@ package bankftest;
 	import java.io.File;
 	import java.io.IOException;
 	import java.util.concurrent.TimeUnit;
+        import org.openqa.selenium.WebElement;
 
 	import org.apache.commons.io.FileUtils;
 	import org.openqa.selenium.By;
