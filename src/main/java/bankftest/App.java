84cb33982c1e610a0b1b83b3ff9944fe43ec46c6
Ashnil <85815731+ashnike@users.noreply.github.com>
2023-06-04 14:31:47 +0530

    Update App.java

--- src/main/java/bankftest/App.java
+++ src/main/java/bankftest/App.java
@@ -33,9 +33,9 @@ package bankftest;
 	        driver.findElement(By.cssSelector(".mail_section input[name='Email']")).sendKeys("ashnil@gmail.com");
 	        driver.findElement(By.cssSelector(".mail_section textarea[name='Message']")).sendKeys("I have a query for money");
 
-                WebElement sendButton = driver.findElement(By.cssSelector(".send_bt a"));
-                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", sendButton);
-                sendButton.click();
+                JavascriptExecutor executor = (JavascriptExecutor) driver;
+                executor.executeScript("arguments[0].click();", element);
+
 
 
 	        String message = driver.findElement(By.cssSelector(".send_bt p")).getText();
